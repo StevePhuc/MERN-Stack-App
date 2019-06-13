6735ef84aef3a478bc5d1616b6e8762efd03640e
Steve Phuc <stevephuc.dev@gmail.com>
2019-06-13 11:15:19 +0300

    heroku see client on serve

---
+++ client/build/precache-manifest.5fa45ea86b0b9ad36f0f6eebc088cbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99beee808924b1dd659e2b411bd3c2de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882920ea0a14e3f222de",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5ef48bd743e0bb2dc236",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "882920ea0a14e3f222de",
+    "url": "/static/js/2.421c501b.chunk.js"
+  },
+  {
+    "revision": "5ef48bd743e0bb2dc236",
+    "url": "/static/js/main.057c6c73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -5,19 +5,25 @@ const PORT = process.env.PORT || 5001;
 const studentRoute = require("./server/routes/student-routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 mongoose.connect(process.env.MONGODB_URI, err => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log("Server is connected to MongoDB Database");
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Server is connected to MongoDB Database");
 });
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1.0", studentRoute);
 
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) => {
+  //   res.send("hello");
+});
+
 app.listen(PORT, () => {
-	console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
