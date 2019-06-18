4faac76e8dd021ca1445ab474c586db9ec99007a
Steve Phuc <stevephuc.dev@gmail.com>
2019-06-18 13:42:09 +0300

    sign in & sign up

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5001;
 const studentRoute = require("./server/routes/student-routes");
+const userRoute = require("./server/routes/user-routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
-mongoose.connect(process.env.MONGODB_URI, err => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, err => {
   if (err) {
     console.log(err);
     return;
@@ -18,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI, err => {
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1.0", studentRoute);
-
+app.use("/api/v1.0/users", userRoute);
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
   //   res.send("hello");
